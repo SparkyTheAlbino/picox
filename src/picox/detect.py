5be3dbc633e51ffa1c061a16340bd645950f9a6e
Harvey <harveythesparky@gmail.com>
2024-04-20 22:11:01 +0100

    Added feature to detect pico devices

---
+++ src/picox/detect.py
@@ -0,0 +1,74 @@
+import glob
+import platform
+from typing import List, Optional
+
+import serial
+import serial.tools.list_ports
+
+from .upy import Pico
+
+
+def get_serial_ports() -> List[str]:
+    """
+    Get USB serial devices for your OS
+    raises:
+        NotImplimentedError - If not Windows, Linux or macOS
+    """
+    match platform.system():
+        case "Windows":
+            ports = serial.tools.list_ports.comports()
+            return [port.device for port in ports if "USB Serial Device" in port.description]
+        case "Linux":
+            return glob.glob('/dev/ttyACM*')
+        case "Darwin":
+            return glob.glob('/dev/tty.usb*')
+        case _:
+            raise NotImplementedError("Unsupported OS")
+
+def is_pico(device: str):
+    """
+    Try to determine if USB serial device is a Pi Pico device
+    args:
+        device (str): USB serial device to test
+    """
+    try:
+        pico = Pico(device, serial_read_timeout=1, serial_write_timeout=1)
+        pico.send_stop_exec()
+        pico.send_enter()
+        return pico.coms_test()
+    except Exception as err:
+        return False
+
+def search_ports_for_pico(serial_devices: List[str]) -> List[str]:
+    """
+    Iterate over list of strings and try to communicate on those USB serial ports with a Pi Pico
+    """
+    for device in serial_devices:
+        if is_pico(device):
+            return device
+    else:
+        return None
+    
+def get_first_pico_serial() -> Optional[str]:
+    """
+    Get ports + detect pico
+    returns:
+        str: serial device name or None
+    """
+    serial_devices = get_serial_ports()
+    for device in serial_devices:
+        if is_pico(device):
+            return device
+    else:
+        return None
+
+def get_all_pico_serial() -> List:
+    """
+    Get ports then return all detected pico devices
+    """
+    serial_devices = get_serial_ports()
+    pico_devices = []
+    for serial_device in serial_devices:
+        if is_pico(serial_device):
+            pico_devices.append(serial_device)
+    return pico_devices
